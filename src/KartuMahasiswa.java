de03096697892cb955fb7ba1fdd0d5ff25dafdbe
Bertrand Raymond Sirait <2581013@unai.edu>
2026-09-18 10:18:54 +0700

    menginput nama lengkap

--- src/KartuMahasiswa.java
+++ src/KartuMahasiswa.java
@@ -5,7 +5,7 @@ public class KartuMahasiswa {
         Scanner input = new Scanner(System.in);
 
         System.out.print("NIK (16 digit) : ");
-        String NIK = input.nextLine();
+        Long NIK = input.nextLong();
 
         System.out.print("Umur           : ");
         int umur = input.nextInt();
@@ -21,7 +21,19 @@ public class KartuMahasiswa {
 
         input.nextLine();
 
+        System.out.print("Nama lengkap    : ");
+        String namaLengkap = input.nextLine();
 
+        System.out.println();
+        System.out.println("===== KARTU MAHASISWA =====");
+        System.out.println("Nama      : " + namaLengkap);
+        System.out.println("NIK       : " + NIK);
+        System.out.println("Umur      : " + umur + " tahun");
+        System.out.println("Tinggi    : " + tinggi + " cm");
+        System.out.println("Gol darah : " + golonganDarah);
+        System.out.println("Menikah   : " + sudahMenikah);
+
+        input.close();
 
 
 
