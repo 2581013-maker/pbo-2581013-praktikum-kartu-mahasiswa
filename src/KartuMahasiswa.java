7f4ef29c67ab17e946c5c730d90af9b3ece86de8
Bertrand Raymond Sirait <2581013@unai.edu>
2026-09-18 09:48:25 +0700

    menginput nik dan umur

---
+++ src/KartuMahasiswa.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class KartuMahasiswa {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("NIK (16 digit) : ");
+        String NIK = input.nextLine();
+
+        System.out.print("Umur           : ");
+        int umur = input.nextLine();
+
+
+
+
+
+    }
+}
